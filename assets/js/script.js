2a477504c86ab00575c3c43ac6f4866403c5de33
Muhammad Sarman Noorlah <sarmanbb@gmail.com>
2023-05-23 01:21:43 +0800

    mini project done

---
+++ assets/js/script.js
@@ -0,0 +1,50 @@
+var button = document.getElementById("submitButton");
+var input = document.getElementById("username");
+
+button.onclick = function() {
+    if (input.value == "") {
+        alert("Please enter a username");
+        return false;
+    }
+};
+
+function hitungLuas() {
+    var sisiLuas = document.getElementsByName("sisi-luas");
+    var luas = sisiLuas[0].value * sisiLuas[0].value;
+    // validation
+    if (sisiLuas[0].value == "") {
+        alert("Silahkan Masukkan Angka")
+        return false;
+    }
+
+    document.getElementById("output_luas").innerHTML = luas;
+    document.getElementById("sisi").innerHTML = sisiLuas[0].value + "x" +
+        sisiLuas[0].value;
+    document.getElementById("output-section-luas").hidden = false;
+}
+
+function resetLuas() {
+    var sisiLuas = document.getElementsByName("sisi-luas");
+    sisiLuas[0].value = "";
+    document.getElementById("output-section-luas").hidden = true;
+}
+
+function hitungKeliling() {
+    var sisiKeliling = document.getElementsByName("sisi-keliling");
+    var keliling = 4 * sisiKeliling[0].value;
+    // validation
+    if (sisiKeliling[0].value == "") {
+        alert("Silahkan Masukkan Angka")
+        return false;
+    }
+    document.getElementById("output_keliling").innerHTML = keliling;
+    document.getElementById("sisi-keliling").innerHTML = "4 x " +
+        sisiKeliling[0].value;
+    document.getElementById("output-section-keliling").hidden = false;
+}
+
+function resetKeliling() {
+    var sisiKeliling = document.getElementsByName("sisi-keliling");
+    sisiKeliling[0].value = "";
+    document.getElementById("output-section-keliling").hidden = true;
+}
