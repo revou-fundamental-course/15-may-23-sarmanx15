25e07194999ee14f50238efa0ea95053b568ace7
Muhammad Sarman Noorlah <sarmanbb@gmail.com>
2023-05-24 06:06:39 +0800

    remove unused codee

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,14 @@
-var button = document.getElementById("submitButton");
-var input = document.getElementById("username");
-
-button.onclick = function() {
-    if (input.value == "") {
-        alert("Please enter a username");
-        return false;
-    }
-};
-
 function hitungLuas() {
     var sisiLuas = document.getElementsByName("sisi-luas");
     var luas = sisiLuas[0].value * sisiLuas[0].value;
-    // validation
+    // input validation
     if (sisiLuas[0].value == "") {
         alert("Silahkan Masukkan Angka")
         return false;
     }
 
     document.getElementById("output_luas").innerHTML = luas;
-    document.getElementById("sisi").innerHTML = sisiLuas[0].value + "x" +
+    document.getElementById("sisi").innerHTML = sisiLuas[0].value + " x " +
         sisiLuas[0].value;
     document.getElementById("output-section-luas").hidden = false;
 }
@@ -32,7 +22,7 @@ function resetLuas() {
 function hitungKeliling() {
     var sisiKeliling = document.getElementsByName("sisi-keliling");
     var keliling = 4 * sisiKeliling[0].value;
-    // validation
+    // input validation
     if (sisiKeliling[0].value == "") {
         alert("Silahkan Masukkan Angka")
         return false;
